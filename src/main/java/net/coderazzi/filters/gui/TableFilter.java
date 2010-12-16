1341be4946e64d4fffe842d81f5d29b41891a966
coderazzi <lu@coderazzi.net>
2010-12-16 01:00:16 +0100

    OptionsManager mixed into TableFilter

--- src/main/java/net/coderazzi/filters/gui/TableFilter.java
+++ src/main/java/net/coderazzi/filters/gui/TableFilter.java
@@ -27,12 +27,20 @@ package net.coderazzi.filters.gui;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.DefaultRowSorter;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.event.RowSorterEvent;
 import javax.swing.event.RowSorterListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 import net.coderazzi.filters.AndFilter;
@@ -65,8 +73,11 @@ import net.coderazzi.filters.gui.editor.FilterEditor;
  *
  * @author  Luis M Pena - lu@coderazzi.net
  */
-public class TableFilter extends AndFilter implements FilterEditor.OptionsManager
+public class TableFilter extends AndFilter implements TableModelListener, FilterEditor.OptionsManager
 {
+
+    /** The associated table, if any. */
+    JTable table;
 
     /**
      * sendNotifications is used internally as a semaphore to disable 
@@ -82,15 +93,29 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
      */
     private boolean pendingNotifications;
 
-    /** The class performing the autoselection, and following sorter changes * */
+    /** 
+     * The class performing the autoSelection, and following sorter changes.<br>
+     * Note that autoSelection has no relationship with autoOptions, it is
+     * the feature to automatically select a row if the current filter filters
+     * out all the rows but one. 
+     **/
     private AutoSelector autoSelector = new AutoSelector();
 
-    /** class to handle the options **/
-    private OptionsManager optionsManager = new OptionsManager();
+    /** 
+     * The listened model. Note: even if table<>null, this model can be null if
+     * there is not need to listen for model changes (no auto options)
+     **/
+	private TableModel listenedModel;
 
-    /** The associated table, if any. */
-    JTable table;
+	/**  The autoOptions mode **/
+	private AutoOptions autoOptions=FilterSettings.autoOptions;
 
+	/** editors with not DISABLED autoOptions */
+	private Map<FilterEditor, AutoOptions> autoEditors =  new HashMap<FilterEditor, AutoOptions>();
+	
+	/** All the editors, mapped by their filter position */
+	private Map<Integer, FilterEditor> editors = new HashMap<Integer, FilterEditor>();
+	
     /**
      * Default constructor
      */
@@ -99,8 +124,7 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
         // create an observer instance to notify the associated table when there
         // are filter changes.
         addFilterObserver(new IFilterObserver() {
-                @Override
-				public void filterUpdated(IFilter obs) {
+                @Override public void filterUpdated(IFilter obs) {
                     notifyUpdatedFilter(false);
                 }
             });
@@ -114,25 +138,33 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
         JTable oldTable = this.table;
         this.table = table;
         autoSelector.replacedTable(oldTable, table);
-        optionsManager.setTable(table);
+		editors.clear();
+		autoEditors.clear();
+		if (listenedModel!=null){
+			listenedModel.removeTableModelListener(this);
+			listenedModel = null;
+		}
     }
 
     /** Returns the associated table */
-    @Override
-	public JTable getTable() {
+    @Override public JTable getTable() {
         return table;
     }
     
     /** Adds a new filter editor */
     public void addFilterEditor(FilterEditor editor) {
     	super.addFilter(editor.getFilter());
-    	optionsManager.addFilterEditor(editor);
+		editors.put(editor.getFilterPosition(), editor);
+		setOptions(editor, autoOptions);
     }
 
     /** Removes an existing editor */
     public void removeFilterEditor(FilterEditor editor) {
     	super.removeFilter(editor.getFilter());
-    	optionsManager.removeFilterEditor(editor);
+		if (editor==editors.get(editor.getFilterPosition())){
+			updateAutoEditors(editor, AutoOptions.DISABLED);
+			editors.remove(editor.getFilterPosition());
+		}
     }
 
     /**
@@ -153,26 +185,42 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
         return sendNotifications >= 0;
     }
     
+	/** Returns editor' auto options mode*/
     @Override public AutoOptions getAutoOptions(FilterEditor editor) {
-    	return optionsManager.getAutoOptions(editor);
+		AutoOptions ret = autoEditors.get(editor);
+		return ret==null? AutoOptions.DISABLED : ret;
     }
     
     @Override public void setOptions(FilterEditor editor, AutoOptions autoOptions) {
-    	optionsManager.setOptions(editor, autoOptions);
+		//this check is needed to avoid setting the options for the
+		//same column multiple times when is created. The creation of
+		//a filter editor is initially done, then the parser is set,
+		//and both operations would trigger this operation
+		if (editor==editors.get(editor.getFilterPosition())){
+			autoOptions=fillOptions(editor, autoOptions);
+			updateAutoEditors(editor, autoOptions);
+		}
     }
     
 	/** Sets/unsets the auto options flag */
     public void setAutoOptions(AutoOptions mode){
-    	if (mode!=getAutoOptions()){
+    	if (mode!=autoOptions){
 	        enableNotifications(false);
-	    	optionsManager.setAutoOptions(mode);
+			this.autoOptions=mode;
+			for (FilterEditor editor : editors.values()){			
+				//it is probably not really needed to set it to null
+				//a better solution would be verifying if the current content
+				//matches any of the options
+				editor.setContent(null);
+				setOptions(editor, mode);
+			}
 	        enableNotifications(true);
     	}
     }
     
 	/** Returns the auto options mode */
 	public AutoOptions getAutoOptions(){
-		return optionsManager.getAutoOptions();
+		return autoOptions;
 	}
 	
     /**
@@ -221,6 +269,126 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
         return false;
     }
     
+	private void updateAutoEditors(FilterEditor editor, AutoOptions newMode){
+		if (newMode==AutoOptions.DISABLED){
+			autoEditors.remove(editor);
+			if (listenedModel!=null && !requiresListener()){
+				listenedModel.removeTableModelListener(this);
+				listenedModel = null;				
+			}
+		} else {
+			autoEditors.put(editor, newMode);
+			if (listenedModel==null && table!=null && requiresListener(newMode)){
+				listenedModel = table.getModel();
+				listenedModel.addTableModelListener(this);				
+			}
+		}
+	}
+	
+	private boolean requiresListener(){
+		for (AutoOptions mode : autoEditors.values()){
+			if (requiresListener(mode)){
+				return true;
+			}			
+		}
+		return false;
+	}
+	
+	private boolean requiresListener(AutoOptions mode){
+		return mode==AutoOptions.EXACT || mode==AutoOptions.EXTENDED;
+	}
+	
+	/**
+	 * Fills the options in the given editor, using the provided AutoOptions
+	 * mode<br>
+	 * It is returned the real options mode set (it only changes for
+	 * enumerations and booleans, where setting EXTENDED is equivalent to BASIC)
+	 */
+	private AutoOptions fillOptions(FilterEditor editor, AutoOptions autoOptions){
+        List<Object> options;
+        if (autoOptions==AutoOptions.DISABLED){
+        	options=Collections.emptyList();
+        } else if (autoOptions==AutoOptions.EXACT){
+        	options = getModelContents(editor);
+        } else {
+            Class<?> c = table.getModel().getColumnClass(editor.getFilterPosition());
+            if (c.equals(Boolean.class)){
+            	autoOptions=AutoOptions.BASIC;
+            	options = new ArrayList<Object>(3);
+            	options.add(true);
+            	options.add(false);
+            } else if (c.isEnum()){
+            	autoOptions=AutoOptions.BASIC;
+            	Object[] enums = c.getEnumConstants();
+            	options = new ArrayList<Object>(enums.length+1);
+            	for (Object each : enums){
+            		options.add(each);
+            	}
+            } else if (autoOptions==AutoOptions.BASIC){
+            	options=Collections.emptyList();
+            	autoOptions = AutoOptions.DISABLED;
+            } else {
+            	options = getModelContents(editor);
+            }        	
+        }
+        editor.setOptions(options);
+        if (autoOptions==AutoOptions.BASIC){
+        	editor.setEditable(false);
+        	if (options.size()<=8){
+        		editor.setMaxHistory(0);
+        	}
+        }
+    	return autoOptions;
+	}
+	
+	/** TableModelListener interface */
+	@Override public void tableChanged(TableModelEvent e) {
+		int firstRow = e.getFirstRow();		
+		if (firstRow != TableModelEvent.HEADER_ROW){
+			int lastRow = e.getLastRow();
+			int c = e.getColumn();
+			if (c == TableModelEvent.ALL_COLUMNS){
+				for (Map.Entry<FilterEditor, AutoOptions> entry : autoEditors.entrySet()){
+					updateColumn(entry.getKey(), entry.getValue(), firstRow, lastRow, e.getType());
+				}
+			} else {
+				FilterEditor editor = editors.get(c);
+				if (editor!=null){
+					updateColumn(editor, autoEditors.get(editor), firstRow, lastRow, e.getType());
+				}
+			}
+		}
+	}
+	
+	private void updateColumn(FilterEditor editor, AutoOptions mode, int firstRow, int lastRow, int eventType){
+		if (mode!=AutoOptions.BASIC){
+			if (eventType==TableModelEvent.INSERT || (eventType==TableModelEvent.UPDATE && mode==AutoOptions.EXTENDED)){
+				editor.addOptions(getModelContents(editor, firstRow, lastRow));
+			} else if (mode==AutoOptions.EXACT){
+				editor.setOptions(getModelContents(editor));
+			}
+		}
+	}
+	
+    private List<Object> getModelContents(FilterEditor editor){
+    	return getModelContents(editor, 0, table.getModel().getRowCount()-1);
+    }
+    
+    private List<Object> getModelContents(FilterEditor editor, 
+    		                                   int firstRow, 
+    		                                   int lastRow){
+    	List<Object> all = new ArrayList<Object>();
+        int column = editor.getFilterPosition();
+        TableModel regModel = table.getModel();
+
+        lastRow = Math.min(regModel.getRowCount() - 1, lastRow);
+
+        while (lastRow >= firstRow) {
+            all.add(regModel.getValueAt(firstRow++, column));
+        }
+        return all;
+    }
+
     /**
      * <p>Class performing the auto selection.</p> 
      * <p>Note that it depends only on the model, and not on the filters.<br>
@@ -235,29 +403,23 @@ public class TableFilter extends AndFilter implements FilterEditor.OptionsManage
         
         /** Autoselection mode * */
         boolean autoSelection = FilterSettings.autoSelection;
-
+        
 		public void replacedTable(JTable oldTable,
                                   JTable newTable) {
-            if (oldTable != null) {
-                oldTable.removePropertyChangeListener(this);
+			String EVENTS[]={"rowSorter", "model"};
+        	for (String e : EVENTS){
+                if (oldTable != null) {
+            		oldTable.removePropertyChangeListener(e, this);
+            	}
+                if (newTable != null) {
+            		newTable.addPropertyChangeListener(e, this);
+                }
             }
-            if (newTable == null) {
-                setSorter(null);
-            } else {
-                newTable.addPropertyChangeListener("rowSorter", this);
-                newTable.addPropertyChangeListener("model", this);
-                setSorter(newTable);
-//                if (sorter == null) {
-//                    newTable.setAutoCreateRowSorter(true);
-//                }
-            }
+            setSorter(newTable);
         }
 
         @Override
         public void propertyChange(PropertyChangeEvent evt) {
-            // next sentence can cause a class cast exception.
-            // that is okay, there is no sense on using the filter
-            // if the row sorter does not admit filtering
             setSorter((JTable) evt.getSource());
         }
 
